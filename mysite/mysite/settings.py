3e5a361cf1771909ff1bebbd68740ac323a94c5c
Matt <matt.@Matts-MacBook-Pro.local>
2024-11-20 18:48:50 +0200

    user can log in and log out

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django_project/settings.py
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/polls/')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
